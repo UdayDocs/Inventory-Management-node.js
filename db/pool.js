950e84e352540aa58b644b61145dc94cbc1b0669
UdayDocs <137787964+UdayDocs@users.noreply.github.com>
2025-03-20 21:57:07 +0530

    env1  deploy commit

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+const connectionString = process.env.DB_CONNECTION || 
+  `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
-  connectionString: process.env.DB_CONNECTION || 
-    `postgresql://${encodeURIComponent(process.env.DB_USERNAME)}:${encodeURIComponent(process.env.DB_PASSWORD)}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+  connectionString: connectionString,
   ssl: process.env.NODE_ENV === 'production' ? { 
     rejectUnauthorized: false 
   } : false
